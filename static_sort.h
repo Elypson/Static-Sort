468939b6ae7aed12469fcec08b32798da6aa0c07
MichaelV <eisflamme@mihahome.de>
2020-04-08 17:28:03 +0200

    added version with second template parameter for comparator

--- static_sort.h
+++ static_sort.h
@@ -23,23 +23,23 @@
  * \tparam T            The element type.
  * \tparam Compare      A comparator functor class that returns true if lhs < rhs.
  */
-template <unsigned NumElements, class Compare = void> class StaticSort
+template <unsigned NumElements, typename Compare> class StaticSort
 {
 	template <class A, class C> struct Swap
 	{
-		template <class T> inline void s(T &v0, T &v1)
+		template <class T> inline void s(T &v0, T &v1, Compare compare)
 		{
-			T t = Compare()(v0, v1) ? v0 : v1; // Min
-			v1 = Compare()(v0, v1) ? v1 : v0; // Max
+			T t = compare(v0, v1) ? v0 : v1; // Min
+			v1 = compare(v0, v1) ? v1 : v0; // Max
 			v0 = t;
 		}
 		
-		inline Swap(A &a, const int &i0, const int &i1) { s(a[i0], a[i1]); }
+		inline Swap(A &a, const int &i0, const int &i1, Compare compare) { s(a[i0], a[i1], compare); }
 	};
 	
 	template <class A> struct Swap <A, void>
 	{
-		template <class T> inline void s(T &v0, T &v1)
+		template <class T> inline void s(T &v0, T &v1, Compare compare)
 		{
 			// Explicitly code out the Min and Max to nudge the compiler
 			// to generate branchless code.
@@ -48,49 +48,49 @@ template <unsigned NumElements, class Compare = void> class StaticSort
 			v0 = t;
 		}
 		
-		inline Swap(A &a, const int &i0, const int &i1) { s(a[i0], a[i1]); }
+		inline Swap(A &a, const int &i0, const int &i1, Compare compare) { s(a[i0], a[i1], compare); }
 	};
 	
 	template <class A, class C, int I, int J, int X, int Y> struct PB
 	{
-		inline PB(A &a)
+		inline PB(A &a, Compare compare)
 		{
 			enum { L = X >> 1, M = (X & 1 ? Y : Y + 1) >> 1, IAddL = I + L, XSubL = X - L };
-			PB<A, C, I, J, L, M> p0(a);
-			PB<A, C, IAddL, J + M, XSubL, Y - M> p1(a);
-			PB<A, C, IAddL, J, XSubL, M> p2(a);
+			PB<A, C, I, J, L, M> p0(a, compare);
+			PB<A, C, IAddL, J + M, XSubL, Y - M> p1(a, compare);
+			PB<A, C, IAddL, J, XSubL, M> p2(a, compare);
 		}
 	};
 	
 	template <class A, class C, int I, int J> struct PB <A, C, I, J, 1, 1>
 	{
-		inline PB(A &a) { Swap<A, C> s(a, I - 1, J - 1); }
+		inline PB(A &a, Compare compare) { Swap<A, C> s(a, I - 1, J - 1, compare); }
 	};
 	
 	template <class A, class C, int I, int J> struct PB <A, C, I, J, 1, 2>
 	{
-		inline PB(A &a) { Swap<A, C> s0(a, I - 1, J); Swap<A, C> s1(a, I - 1, J - 1); }
+		inline PB(A &a, Compare compare) { Swap<A, C> s0(a, I - 1, J, compare); Swap<A, C> s1(a, I - 1, J - 1, compare); }
 	};
 	
 	template <class A, class C, int I, int J> struct PB <A, C, I, J, 2, 1>
 	{
-		inline PB(A &a) { Swap<A, C> s0(a, I - 1, J - 1); Swap<A, C> s1(a, I, J - 1); }
+		inline PB(A &a, Compare compare) { Swap<A, C> s0(a, I - 1, J - 1, compare); Swap<A, C> s1(a, I, J - 1, compare); }
 	};
 	
 	template <class A, class C, int I, int M, bool Stop = false> struct PS
 	{
-		inline PS(A &a)
+		inline PS(A &a, Compare compare)
 		{
 			enum { L = M >> 1, IAddL = I + L, MSubL = M - L};
-			PS<A, C, I, L, (L <= 1)> ps0(a);
-			PS<A, C, IAddL, MSubL, (MSubL <= 1)> ps1(a);
-			PB<A, C, I, IAddL, L, MSubL> pb(a);
+			PS<A, C, I, L, (L <= 1)> ps0(a, compare);
+			PS<A, C, IAddL, MSubL, (MSubL <= 1)> ps1(a, compare);
+			PB<A, C, I, IAddL, L, MSubL> pb(a, compare);
 		}
 	};
 	
 	template <class A, class C, int I, int M> struct PS <A, C, I, M, true>
 	{
-		inline PS(A &a) {}
+		inline PS(A &a, Compare compare) {}
 	};
 	
 public:
@@ -98,18 +98,18 @@ public:
 	 * Sorts the array/container arr.
 	 * \param  arr  The array/container to be sorted.
 	 */
-	template <class Container> inline void operator() (Container &arr) const
+	template <class Container> inline void operator() (Container &arr, Compare compare) const
 	{
-		PS<Container, Compare, 1, NumElements, (NumElements <= 1)> ps(arr);
+		PS<Container, Compare, 1, NumElements, (NumElements <= 1)> ps(arr, compare);
 	};
 
 	/**
 	 * Sorts the array arr.
 	 * \param  arr  The array to be sorted.
 	 */
-	template <class T> inline void operator() (T *arr) const
+	template <class T> inline void operator() (T *arr, Compare compare) const
 	{
-		PS<T*, Compare, 1, NumElements, (NumElements <= 1)> ps(arr);
+		PS<T*, Compare, 1, NumElements, (NumElements <= 1)> ps(arr, compare);
 	};
 };
 #endif
